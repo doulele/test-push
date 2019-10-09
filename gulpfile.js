eab4957b3b462e3b55d5e89ab01c30639f0f32fb
野生小猿猿 <1220736335@qq.com>
2019-10-09 16:46:22 +0800

    这是第一次自主将本地文件传至github

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+var gulp=require('gulp');
+var open=require('open');
+var $=require('gulp-load-plugins')();
+
+gulp.task('html',function(){
+	return gulp.src('develop/*html')
+		.pipe($.htmlmin({collapseWhitespace:true}))
+		.pipe(gulp.dest('product'))
+		.pipe($.connect.reload())
+})
+
+gulp.task('css',function(){
+	return gulp.src('develop/css/*styl')
+		.pipe($.stylus({compress:true}))
+		.pipe(gulp.dest('product'))
+		.pipe($.connect.reload())
+})
+
+gulp.task('js',function(){
+	return gulp.src('develop/js/*.js')
+		.pipe($.uglify())
+		.pipe($.concat('all.js'))
+		.pipe(gulp.dest('product'))
+		.pipe($.connect.reload())
+})
+
+gulp.task('image',function(){
+	return gulp.src('develop/image/*')
+		.pipe($.imagemin())
+		.pipe(gulp.dest('product/image'))
+		.pipe($.connect.reload())
+})
+
+//自动加载
+gulp.task('watch',function(){
+	gulp.watch('develop/*html',gulp.series('html'));
+	gulp.watch('develop/css/*styl',gulp.series('css'));
+	gulp.watch('develop/js/*js',gulp.series('js'));
+})
+gulp.task('connect',function(){
+	$.connect.server({
+		root:'product/',
+		livereload:true,
+		port:9999
+	})
+})
+
+//自动打开
+open("http://localhost:9999/test.html");
+
+//清理
+gulp.task('clean',function(){
+	return gulp.src('product/*js')
+		.pipe($.clean())
+})
+gulp.task('default',gulp.series('clean',gulp.parallel('connect','watch','image','css','js','html')))
---
+++ product/all.js
@@ -0,0 +1,2 @@
+document.getElementsByTagName("div")[0].style.backgroundColor="blue";
+for(var oImg=document.getElementsByTagName("img"),i=0;i<oImg.length;i++)!function(o){oImg[o].onmouseover=function(){oImg[o].style.opacity=".7"},oImg[o].onmouseout=function(){oImg[o].style.opacity="1"}}(i);
